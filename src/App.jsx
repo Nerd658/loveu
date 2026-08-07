af8d4563f1239f463daff60636accd531be49820
Nerd658 <m9bikienga@gmail.com>
2026-08-07 11:35:41 +0000

    refactor: remove all pages except Ask invitation

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
-import MainLayout from './layouts/MainLayout'
-import Home from './pages/Home'
-import Timeline from './pages/Timeline'
-import Coach from './pages/Coach'
-import Planning from './pages/Planning'
-import Settings from './pages/Settings'
 import Ask from './pages/Ask'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route path="timeline" element={<Timeline />} />
-        <Route path="coach" element={<Coach />} />
-        <Route path="planning" element={<Planning />} />
-        <Route path="settings" element={<Settings />} />
-        <Route path="ask" element={<Ask />} />
-      </Route>
+      <Route path="/" element={<Ask />} />
     </Routes>
   )
 }
